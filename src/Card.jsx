fea5ab98bdd36fbd4c729eddc146bf9f0d2a69f5
Judit Kisistok <judit.kisistok@gmail.com>
2026-03-06 17:41:33 +0100

    initial commit

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card({id, name, type, hp, attack}) {
+    return (
+      <div>
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={name} />
+        <h1>{name}</h1>
+        <p>{type}</p>
+        <p>{hp}</p>
+        <p>{attack}</p>
+      </div>
+    )
+  }
