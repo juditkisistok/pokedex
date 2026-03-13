8b8d8515d1b9d69a282646928aeef728b53befc6
Judit Kisistok <judit.kisistok@gmail.com>
2026-03-13 23:42:50 +0100

    fix logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import "./App.css";
 import CardGrid from "./CardGrid";
 import Filter from "./Filter";
 import { useState } from "react";
+import logo from "./assets/pokemon-logo.png";
 
 function App() {
   const [currentType, setCurrentType] = useState("all");
   const [favorites, setFavorites] = useState([]);
   return (
     <>
-    <div className="topbar">
-      <header>
-        <img src="/pokemon-logo.png" alt="Pokedex" />
-      </header>
-      <Filter currentType={currentType} setCurrentType={setCurrentType} favorites={favorites} />
+      <div className="topbar">
+        <header>
+          <img src={logo} alt="Pokedex" />
+        </header>
+        <Filter
+          currentType={currentType}
+          setCurrentType={setCurrentType}
+          favorites={favorites}
+        />
       </div>
       <CardGrid
         currentType={currentType}
